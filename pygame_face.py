d4b8d2fb1067a920c3fa16b09c8d30dcd0cadcd0
Mark Final <markfinal@hotmail.com>
2025-05-24 20:06:53 +0100

    Draw a face with pygame shapes

---
+++ pygame_face.py
@@ -0,0 +1,32 @@
+import math
+import sys
+
+import pygame
+import pygame.locals
+
+width = 640
+height = 480
+two_pi = 2 * 3.14159
+
+screen = pygame.display.set_mode((width, height))
+# background
+screen.fill("black")
+
+# round face
+pygame.draw.circle(screen, "pink", (width / 2, height / 2), 200)
+
+# two eyes
+pygame.draw.circle(screen, "white", (width / 3, height / 3), 50)
+pygame.draw.circle(screen, "black", (width / 3, height / 3), 10)
+pygame.draw.circle(screen, "white", (2 * width / 3, height / 3), 50)
+pygame.draw.circle(screen, "black", (2 * width / 3, height / 3), 10)
+
+# smile
+pygame.draw.arc(screen, "red", (220, 150, 200, 200), math.radians(220), math.radians(320), 10)
+
+pygame.display.flip()
+
+while True:
+    for events in pygame.event.get():
+        if events.type == pygame.locals.QUIT:
+            sys.exit(0)
